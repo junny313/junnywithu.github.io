729d01f364709432c4611b95c32630e3666cac7e
Muhammad_junaid <88631451+junny313@users.noreply.github.com>
2021-09-19 23:29:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function() {
+    $(window).scroll(function() {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function() {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -56,18 +56,50 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
     });
 });
+
+function sendmail() {
+
+    const name = $('#name').val();
+    const email = $('#sender').val();
+    const phonenumber = $('#phonenumber').val();
+    const message = $('#message').val();
+
+    const Body ='Name: '+name+'<br>Email:' +email+'<br>phonenumber: '+phonenumber+'<br>Message: '+message;
+
+}
+
+
+Email.send({
+    Host: "smtp.gmail.com",
+    Username: "theislam00980@gmail.com",
+    Password: "zong4G(html)",
+    To: 'official00980@gmail.com',
+    From: "theislam00980@gmail.com",
+    Subject: "New mail on junnywithu site From: "+name,
+    Body : Body
+}).then(
+    message =>{
+        if(message=='Ok'){
+            alert("Your message has been send.Thank you for connecting.");
+        }
+        else{
+            console.error(message);
+            alert("There is error at sending message.Please try again later.");
+        }
+    }
+);
